16bd3497e9869c1fa1b852f3e8c82b9f1eeb36fa
Takuto Hayashi <takuto.hayashi@gmail.com>
2013-05-10 16:56:19 +0900

    Use while loop

--- main.ml
+++ main.ml
@@ -2,37 +2,48 @@
 open Printf
 
 let rec read buf = begin
-  eprintf "> ";
-  flush stderr;
-  let str = read_line () in
-  let len = String.length str in
-  if len >= 2 && String.sub str (len-2) 2 = ";;" then begin
-    Buffer.add_string buf (String.sub str 0 (len-2));
-    Buffer.contents buf
-  end
-  else begin
-    Buffer.add_string buf str;
-    read buf
-  end
-end
-  
-let rec repl inf = begin
-  let buf = Buffer.create 256 in
-  let str = read buf in
-  let parser = Parser.of_string str in
   begin try
-    let top = Parser.parse parser in
-    let (scm, inf) = Inferrer.infer_top inf top in
-    eprintf "%s\n" (Scheme.show scm);
-    repl inf
+    while true do
+      eprintf "> ";
+      flush stderr;
+      let str = read_line () in
+      let len = String.length str in
+      if len >= 2 && String.sub str (len-2) 2 = ";;" then begin
+        Buffer.add_string buf (String.sub str 0 (len-2));
+        raise End_of_file
+      end
+      else begin
+        Buffer.add_string buf str
+      end
+    done
   with
     | End_of_file -> ()
-  end
+  end;
+  Buffer.contents buf
+end
+
+let rec repl () = begin
+  let infref = ref Inferrer.empty in
+  while true do
+    let buf = Buffer.create 256 in
+    let str = read buf in
+    let parser = Parser.of_string str in
+    begin try
+      let top = Parser.parse parser in
+      let (scm, inf) = Inferrer.infer_top !infref top in begin
+      infref := inf;
+      eprintf "%s\n" (Scheme.show scm)
+      end
+    with
+      | Failure(message) ->
+        eprintf "%s\n" message
+    end
+  done
 end
 
 let rec main () =
   begin try
-    repl Inferrer.empty
+    repl ()
   with
     | End_of_file -> ()
   end
