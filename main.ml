323b7b2d34e6535ccae1834b493a67140824a8e7
Takuto Hayashi <takuto.hayashi@gmail.com>
2013-05-10 16:29:43 +0900

    Add multi-line read

--- main.ml
+++ main.ml
@@ -1,10 +1,24 @@
 
 open Printf
 
-let rec repl inf = begin
+let rec read buf = begin
   eprintf "> ";
   flush stderr;
   let str = read_line () in
+  let len = String.length str in
+  if len >= 2 && String.sub str (len-2) 2 = ";;" then begin
+    Buffer.add_string buf (String.sub str 0 (len-2));
+    Buffer.contents buf
+  end
+  else begin
+    Buffer.add_string buf str;
+    read buf
+  end
+end
+  
+let rec repl inf = begin
+  let buf = Buffer.create 256 in
+  let str = read buf in
   let parser = Parser.of_string str in
   begin try
     let top = Parser.parse parser in
