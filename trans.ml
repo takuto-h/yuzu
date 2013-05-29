e447ba345e1d4d480f63b46ea2c4ca0d206b038b
Takuto Hayashi <takuto.hayashi@gmail.com>
2013-05-29 12:16:39 +0900

    Add indentation

--- trans.ml
+++ trans.ml
@@ -2,13 +2,21 @@
 open Printf
 
 type t = {
-  dummy : unit;
+  basic_offset : int;
+  indent_level : int;
 }
 
-let create () = {
-  dummy = ();
+let create basic_offset = {
+  basic_offset = basic_offset;
+  indent_level = 0;
 }
 
+let ocaml_basic_offset = 2
+  
+let indent {basic_offset;indent_level} str =
+  let offset = basic_offset * indent_level in
+  sprintf "%s%s" (String.make offset ' ') str
+  
 let translate_ident {Ident.name} =
   name
 
@@ -19,8 +27,9 @@ let rec translate_expr trans = function
     translate_ident ident
   | Expr.Abs(param_ident,body_expr) ->
     let str_param = translate_ident param_ident in
-    let str_body = translate_expr trans body_expr in
-    sprintf "(fun %s -> %s)" str_param str_body
+    let trans_body = {trans with indent_level=trans.indent_level+1} in
+    let str_body = translate_expr trans_body body_expr in
+    sprintf "begin fun %s ->\n%s\nend" str_param (indent trans_body str_body)
   | Expr.App(fun_expr,arg_expr) ->
     let str_fun = translate_expr trans fun_expr in
     let str_arg = translate_expr trans arg_expr in
@@ -40,7 +49,7 @@ let translate_file fname_in fname_out =
       let src = Source.create fname_in chan_in in
       let lexer = Lexer.create src in
       let parser = Parser.create lexer in
-      let trans = create () in
+      let trans = create ocaml_basic_offset in
       while true do
         match Parser.parse parser with
           | None ->
