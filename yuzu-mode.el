ea92b6b4ef16588a2d6b37213c9438680567e0e5
Takuto Hayashi <takuto.hayashi@gmail.com>
2013-06-06 21:42:22 +0900

    Update yuzu-mode.el

--- yuzu-mode.el
+++ yuzu-mode.el
@@ -10,9 +10,18 @@
   (let ((yuzu-mode-font-lock-keywords
          '(("\\<\\(open\\|type\\|def\\|var\\|if\\|else\\|match\\|case\\)\\>" .
             (1 font-lock-keyword-face))
-           ("def\\s +\\(\\(\\sw\\|\\s_\\)+\\)" . (1 font-lock-function-name-face))
-           ("var\\s +\\(\\(\\sw\\|\\s_\\)+\\)" . (1 font-lock-variable-name-face))
-           ("type\\s +\\(\\(\\sw\\|\\s_\\)+\\)" . (1 font-lock-type-face))
+           ("def\\s +\\(\\sw+\\)" . (1 font-lock-function-name-face))
+           ("var\\s +\\(\\sw+\\)" . (1 font-lock-variable-name-face))
+           ("type\\s +\\(\\sw+\\)" . (1 font-lock-type-face))
+           ("\\(\"\\(\\\\[\\\"]\\|[^\"\n]\\)*\"\\)" . (1 font-lock-string-face t))
+           ("\\('\\(\\\\[\\']\\|[^'\n]\\)*'\\)" . (1 font-lock-string-face t))
            )))
-    (setq font-lock-defaults (list yuzu-mode-font-lock-keywords nil nil nil nil)))
+    (setq font-lock-defaults
+          (list
+           yuzu-mode-font-lock-keywords
+           t
+           nil
+           '((?_ . "w"))
+           nil
+           )))
   (add-hook 'yuzu-mode-hook 'turn-on-font-lock))
