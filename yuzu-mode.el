d0c092fcc287615d7300db98801e4539bceb8af8
Takuto Hayashi <takuto.hayashi@gmail.com>
2013-06-07 11:44:18 +0900

    Update yuzu-mode.el

--- yuzu-mode.el
+++ yuzu-mode.el
@@ -8,10 +8,15 @@
 (define-derived-mode yuzu-mode fundamental-mode "Yuzu"
   (make-local-variable 'font-lock-defaults)
   (let ((yuzu-mode-font-lock-keywords
-         '(("\\<\\(open\\|type\\|def\\|var\\|if\\|else\\|match\\|case\\|when\\|mutable\\)\\>" . (1 font-lock-keyword-face))
-           ("def\\s +\\(\\sw+\\)" . (1 font-lock-function-name-face))
-           ("var\\s +\\(\\sw+\\)" . (1 font-lock-variable-name-face))
+         '(("\\<\\(open\\|type\\|def\\|var\\)\\>" . (1 font-lock-function-name-face))
+           ("\\<\\(if\\|else\\|match\\|case\\|when\\|mutable\\)\\>" . (1 font-lock-keyword-face))
            ("type\\s +\\(\\sw+\\)" . (1 font-lock-type-face))
+           ("open\\s +\\(\\sw+\\)" . (1 font-lock-string-face))
+           ("def\\s +\\(\\sw+\\)" . (1 font-lock-variable-name-face))
+           ("var\\s +\\(\\sw+\\)" . (1 font-lock-variable-name-face))
+           ("case\\s +\\(\\sw+\\)" . (1 font-lock-constant-face))
+           ("\\([][!%&()---=~^|`@{+;*:}<,>.?/]\\)" . (1 font-lock-comment-face))
+           ("\\<\\([ABCDEFGHIJKLMNOPQRSTUVWXYZ]\\sw*\\)\\>\\." . (1 font-lock-type-face t))
            ("\\(\"\\(\\\\[\\\"]\\|[^\"\n]\\)*\"\\)" . (1 font-lock-string-face t))
            ("\\('\\(\\\\[\\']\\|[^'\n]\\)*'\\)" . (1 font-lock-string-face t))
            ("\\(//[^\n]*\\)$" . (1 font-lock-comment-face t))
