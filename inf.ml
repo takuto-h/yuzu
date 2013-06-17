5d4f5ce9c44ce5f2a043abd4776590e3706ccefb
Takuto Hayashi <takuto.hayashi@gmail.com>
2013-06-17 14:54:52 +0900

    Rename lit to literal

--- inf.ml
+++ inf.ml
@@ -14,7 +14,7 @@ let string_type = (Type.Con ([], "string"))
 
 let char_type = (Type.Con ([], "char"))
 
-let rec infer_lit = begin fun lit ->
+let rec infer_literal = begin fun lit ->
   begin match lit with
     | (Literal.Int(_)) ->
       int_type
@@ -29,7 +29,7 @@ let rec infer = begin fun inf ->
   begin fun expr ->
     begin match expr with
       | (Expr.Con(lit)) ->
-        (infer_lit lit)
+        (infer_literal lit)
     end
   end
 end
