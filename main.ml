e64bc72ab2c9d48bd45d0787d533beb25eb662c9
Takuto Hayashi <takuto.hayashi@gmail.com>
2013-06-27 13:33:07 +0900

    Move load_iface_file

--- main.ml
+++ main.ml
@@ -2,15 +2,15 @@ open Printf
 
 let rec main = begin fun (() _) ->
   begin let compiler = (Compiler.create ()) in
-  begin let result = begin if ((( = ) (Array.length Sys.argv)) 1) then
-    ((YzOption.bind ((Compiler.load_iface_file compiler) "pervasives.yzi")) begin fun compiler ->
+  begin let result = ((YzOption.bind ((Compiler.load_iface_file compiler) "pervasives.yzi")) begin fun compiler ->
+    begin if ((( = ) (Array.length Sys.argv)) 1) then
       (YzOption.return (Compiler.interactive compiler))
-    end)
-  else
-    begin let fnames = (Array.to_list (((Array.sub Sys.argv) 1) ((( - ) (Array.length Sys.argv)) 1))) in
-    ((Compiler.compile_files compiler) fnames)
+    else
+      begin let fnames = (Array.to_list (((Array.sub Sys.argv) 1) ((( - ) (Array.length Sys.argv)) 1))) in
+      ((Compiler.compile_files compiler) fnames)
+      end
     end
-  end in
+  end) in
   begin match result with
     | (None _) ->
       (exit 1)
