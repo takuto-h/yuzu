f7d06ded2d044844714bd97bb1f8e1be98c614b3
Takuto Hayashi <takuto.hayashi@gmail.com>
2013-07-01 08:44:08 +0900

    Add Compiler.compile_source

--- compiler.ml
+++ compiler.ml
@@ -18,6 +18,40 @@ let rec create = begin fun () ->
   }
 end
 
+let rec compile_source = begin fun compiler ->
+  begin fun src ->
+    begin fun parse ->
+      begin fun compile ->
+        begin let lexer = (Lexer.create src) in
+        begin let parser = (Parser.create lexer) in
+        begin try
+          begin let rec loop = begin fun compiler ->
+            begin match (parse parser) with
+              | None ->
+                (Some compiler)
+              | (Some expr) ->
+                (loop ((compile compiler) expr))
+            end
+          end in
+          (loop compiler)
+          end
+        with
+          | (Failure message) ->
+            begin
+            ((eprintf "%s") message);
+            begin
+            (flush stderr);
+            None
+            end
+            end
+        end
+        end
+        end
+      end
+    end
+  end
+end
+
 let rec load_iface_file = begin fun compiler ->
   begin fun fname_in ->
     begin let chopped = ((Filename.chop_suffix fname_in) ".yzi") in
@@ -25,42 +59,24 @@ let rec load_iface_file = begin fun compiler ->
     ((with_open_in fname_in) begin fun chan_in ->
       begin let strm = (Stream.of_channel chan_in) in
       begin let src = (((Source.create fname_in) strm) Pos.File) in
-      begin let lexer = (Lexer.create src) in
-      begin let parser = (Parser.create lexer) in
       begin let compiler = {
         compiler with
         inf = ((Inf.enter_module compiler.inf) mod_name);
       } in
-      begin try
-        begin let rec loop = begin fun compiler ->
-          begin match (Parser.parse_decl parser) with
-            | None ->
-              (Some {
-                compiler with
-                inf = ((Inf.leave_module compiler.inf) mod_name);
-              })
-            | (Some decl) ->
-              begin let inf = ((Inf.load_decl compiler.inf) decl) in
-              (loop {
-                compiler with
-                inf = inf;
-              })
-              end
-          end
-        end in
-        (loop compiler)
+      begin let status = ((((compile_source compiler) src) Parser.parse_decl) begin fun compiler ->
+        begin fun decl ->
+          {
+            compiler with
+            inf = ((Inf.load_decl compiler.inf) decl);
+          }
         end
-      with
-        | (Failure message) ->
-          begin
-          ((eprintf "%s") message);
-          begin
-          (flush stderr);
-          None
-          end
-          end
-      end
-      end
+      end) in
+      ((YzOption.bind status) begin fun compiler ->
+        (YzOption.return {
+          compiler with
+          inf = ((Inf.leave_module compiler.inf) mod_name);
+        })
+      end)
       end
       end
       end
@@ -80,48 +96,32 @@ let rec compile_file = begin fun compiler ->
       ((with_open_out fname_out) begin fun chan_out ->
         begin let strm = (Stream.of_channel chan_in) in
         begin let src = (((Source.create fname_in) strm) Pos.File) in
-        begin let lexer = (Lexer.create src) in
-        begin let parser = (Parser.create lexer) in
         begin let trans = (Trans.create basic_offset) in
         begin let compiler = {
           compiler with
           inf = ((Inf.enter_module compiler.inf) mod_name);
         } in
-        begin try
-          begin let rec loop = begin fun compiler ->
-            begin match (Parser.parse parser) with
-              | None ->
-                (Some {
-                  compiler with
-                  inf = ((Inf.leave_module compiler.inf) mod_name);
-                })
-              | (Some top) ->
-                begin let (inf, decl) = ((Inf.infer_top compiler.inf) top) in
-                begin let result = ((Trans.translate_top trans) top) in
-                begin
-                (((fprintf chan_out) "%s\n") result);
-                (loop {
-                  compiler with
-                  inf = inf;
-                })
-                end
-                end
-                end
+        begin let status = ((((compile_source compiler) src) Parser.parse) begin fun compiler ->
+          begin fun top ->
+            begin let (inf, decl) = ((Inf.infer_top compiler.inf) top) in
+            begin let result = ((Trans.translate_top trans) top) in
+            begin
+            (((fprintf chan_out) "%s\n") result);
+            {
+              compiler with
+              inf = inf;
+            }
             end
-          end in
-          (loop compiler)
+            end
+            end
           end
-        with
-          | (Failure message) ->
-            begin
-            ((eprintf "%s") message);
-            begin
-            (flush stderr);
-            None
-            end
-            end
-        end
-        end
+        end) in
+        ((YzOption.bind status) begin fun compiler ->
+          (YzOption.return {
+            compiler with
+            inf = ((Inf.leave_module compiler.inf) mod_name);
+          })
+        end)
         end
         end
         end
@@ -142,46 +142,30 @@ let rec compile_string = begin fun compiler ->
       begin let buf_output = (Buffer.create initial_buffer_size) in
       begin let strm = (Stream.of_string str) in
       begin let src = (((Source.create fname_in) strm) (Pos.String str)) in
-      begin let lexer = (Lexer.create src) in
-      begin let parser = (Parser.create lexer) in
       begin let trans = (Trans.create basic_offset) in
-      begin try
-        begin let rec loop = begin fun compiler ->
-          begin match (Parser.parse parser) with
-            | None ->
-              (Some (compiler, (Buffer.contents buf_decls), (Buffer.contents buf_output)))
-            | (Some top) ->
-              begin let (inf, decls) = ((Inf.infer_top compiler.inf) top) in
-              begin
-              ((List.iter begin fun decl ->
-                ((Buffer.add_string buf_decls) (Decl.show decl))
-              end) decls);
-              begin let result = ((Trans.translate_top trans) top) in
-              begin
-              ((Buffer.add_string buf_output) result);
-              (loop {
-                compiler with
-                inf = inf;
-              })
-              end
-              end
-              end
-              end
+      begin let status = ((((compile_source compiler) src) Parser.parse) begin fun compiler ->
+        begin fun top ->
+          begin let (inf, decls) = ((Inf.infer_top compiler.inf) top) in
+          begin
+          ((List.iter begin fun decl ->
+            ((Buffer.add_string buf_decls) (Decl.show decl))
+          end) decls);
+          begin let result = ((Trans.translate_top trans) top) in
+          begin
+          ((Buffer.add_string buf_output) result);
+          {
+            compiler with
+            inf = inf;
+          }
           end
-        end in
-        (loop compiler)
+          end
+          end
+          end
         end
-      with
-        | (Failure message) ->
-          begin
-          ((eprintf "%s") message);
-          begin
-          (flush stderr);
-          None
-          end
-          end
-      end
-      end
+      end) in
+      ((YzOption.bind status) begin fun compiler ->
+        (YzOption.return (compiler, (Buffer.contents buf_decls), (Buffer.contents buf_output)))
+      end)
       end
       end
       end
